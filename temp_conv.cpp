bdc5b88caa9eb5d3ddfd837e9196ff3678785468
Sokaneil Sieng <sokaneilsieng0505@gmail.com>
2023-09-28 09:48:13 +0200

    chapter 1

--- temp_conv.cpp
+++ temp_conv.cpp
@@ -8,17 +8,21 @@ void temp_read_convert(std::istream &input, std::ostream &output)
 
     input >> temp >> unit;
     float cel;
-    if (unit == 'K') {
-        cel = temp - 273.15;
-    } else if (unit == 'F') {
-        cel = (temp - 32) * (5.0 / 9.0);
-    } else {
-        cel = temp;
-    }
-    float kal  = cel + 273.15;
-    float fahr = (cel * 9.0 / 5.0) + 32;
+    if (unit == 'K' || unit == 'F' || unit == 'C') {
+        if (unit == 'K') {
+            cel = temp - 273.15;
+        } else if (unit == 'F') {
+            cel = (temp - 32) * (5.0 / 9.0);
+        } else {
+            cel = temp;
+        }
+        float kal  = cel + 273.15;
+        float fahr = (cel * 9.0 / 5.0) + 32;
 
-    output << cel << " C" << std::endl;
-    output << fahr << " F" << std::endl;
-    output << kal << " K" << std::endl;
+        output << cel << " C" << std::endl;
+        output << fahr << " F" << std::endl;
+        output << kal << " K" << std::endl;
+    } else {
+        std::cerr << "Error : Unit Incorrect" << std::endl;
+    }
 }
